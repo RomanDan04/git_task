72441ed6c729bd47bd4081d92401e94e787ade58
Dan Roman <crme012@cedmd.ced.it>
2023-02-21 12:33:30 +0200

    modificare routing, adaugare pagina login

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import { AuthGuard } from '../services/AuthGuard.service';
+import { HomeComponent } from '../components/home/home.component';
+import { NewsComponent } from '../components/news/news.component';
+import { ProfileComponent } from '../components/profile/profile.component';
+import { LoginComponent } from '../components/login/login.component';
+import { NotfoundComponent } from '../components/notfound/notfound.component';
 
 import { Routes, RouterModule } from '@angular/router';
 
 
 
 const appRotues: Routes = [
-  // {path: '', component: HomeComponent},
-  // {path: 'news', component: NewsComponent},
-  // {path: 'login', component: LoginComponent},
-  // {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
-  // {path: '**', component: NotfoundComponent}
+  {path: '', component: HomeComponent},
+  {path: 'news', component: NewsComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
+  {path: '**', component: NotfoundComponent}
 ]
 
 @NgModule({
